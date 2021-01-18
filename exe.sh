3816e612eaeb93863ab1c6d8f19fe863b600d47a
Bumsoo Kim <cmng828rhuypqq@cau.ac.kr>
2021-01-19 00:35:30 +0900

    Create Quick Setup shell file

---
+++ exe.sh
@@ -0,0 +1,11 @@
+echo "linekd with www.processing.org/download...loading"
+
+start chrome https://processing.org/download/
+
+:<<'END'
+linked with www.processing.org/download
+
+- windows -> https://download.processing.org/processing-3.5.4-windows64.zip
+- linux -> https://download.processing.org/processing-3.5.4-linux64.tgz
+- Mac OS -> https://download.processing.org/processing-3.5.4-macosx.zip
+END
