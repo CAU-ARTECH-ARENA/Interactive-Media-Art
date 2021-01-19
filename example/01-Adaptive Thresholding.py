2bfe90766381024ab753d09251e73ccd056d8844
Bumsoo Kim <cmng828rhuypqq@cau.ac.kr>
2021-01-19 17:22:59 +0900

    Create Python Example 01-Adaptive Thresholding.py

---
+++ example/01-Adaptive Thresholding.py
@@ -0,0 +1,18 @@
+import numpy as np
+import cv2
+ 
+detector = cv2.CascadeClassifier('C:/Users/GRlab/Desktop/haarcascade_frontalface_default.xml')
+cap = cv2.VideoCapture(0)
+ 
+while (True):
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    binary = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                   cv2.THRESH_BINARY, 25, 5)
+ 
+    cv2.imshow('frame', binary)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+ 
+cap.release()
+cv2.destroyAllWindows()
