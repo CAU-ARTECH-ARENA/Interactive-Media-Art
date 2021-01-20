6b13fbf178a68ecccf124e4f8296e12f18f44fe2
Bumsoo Kim <cmng828rhuypqq@cau.ac.kr>
2021-01-20 14:27:20 +0900

    Create JavaScript Example 01-Thresholding.js

---
+++ example/01-Thresholding.js
@@ -0,0 +1,15 @@
+let cam;
+
+function setup() {
+  createCanvas(640, 480);
+  frameRate(30);
+  
+  cam = createCapture(VIDEO);
+  cam.size(640,480);
+  cam.hide();
+}
+
+function draw() {
+  image(cam, 0, 0);
+  filter(THRESHOLD);
+}
