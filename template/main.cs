b958caf63ec8ff09d70319b2c0962988caae943a
Bumsoo Kim <cmng828rhuypqq@cau.ac.kr>
2021-01-20 19:33:44 +0900

    Create C# Template_v00

---
+++ template/main.cs
@@ -0,0 +1,40 @@
+namespace OpenCV_Sample
+{
+    public partial class Form1 : Form
+    {
+        VideoCapture video;
+        Mat frame = new Mat();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                video = new VideoCapture(0);
+                video.FrameWidth = 640;
+                video.FrameHeight = 480;
+            }
+            catch
+            {
+                timer1.Enabled = false;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            video.Read(frame);
+            pictureBoxIpl1.ImageIpl = frame;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            frame.Dispose();
+        }
+
+    }
+
+}
