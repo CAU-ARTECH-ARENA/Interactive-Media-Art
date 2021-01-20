f6fc443f2cce673d54ab28dbe8c2256c8f7fa471
Bumsoo Kim <cmng828rhuypqq@cau.ac.kr>
2021-01-20 11:38:30 +0900

    Add Auto MakeDir and Move Code files

--- exe.sh
+++ exe.sh
@@ -2,6 +2,20 @@ echo "linekd with www.processing.org/download...loading"
 
 start chrome https://processing.org/download/
 
+cd example
+
+find . -name '*.pde' -exec mv {} ../../../../Desktop \;
+find . -name '*.py' -exec mv {} ../../../../Desktop \;
+
+echo "move pde and py files to Desktop Directory"
+
+cd ../../../../Desktop
+mkdir 01-Thresholding
+mv 01-Thresholding.pde
+
+mkdir example_python
+find . -name '*.py' -exec mv {} example_python \;
+
 :<<'END'
 linked with www.processing.org/download
 
